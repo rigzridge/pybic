69b4ae59a07e0e0992cf4b058d21fe787928922c
rigzridge <75751275+rigzridge@users.noreply.github.com>
2022-07-05 03:12:30 -0400

    Add files via upload

--- pybic.py
+++ pybic.py
@@ -1,47 +1,99 @@
-# Quick attempt at PyBic
-# 6/24/2022
+class BicAn:
+# Bicoherence analysis class for DSP
+    
+    FontSize  = 20
+    WarnSize  = 1000
+    MaxRes    = []
+    Samples   = []
+    NFreq     = []
+    RunBicAn  = False
+    NormToNyq = False
+    Nseries   = []
+    WinVec    = []
 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import time
+    Raw       = []
+    Processed = []
+    History   = ' '
+    SampRate  = 1
+    FreqRes   = 0
+    SubInt    = 512
+    Step      = 128
+    Window    = 'hann'       
+    Sigma     = 1
+    JustSpec  = False
+    SpecType  = 'stft'
+    ErrLim    = 1e15
+    FScale    = 0
+    TScale    = 0
+    Filter    = 'none'
+    Bispectro = False
+    Smooth    = 1
+    PlotIt    = False
+    LilGuy    = 1e-6
+    SizeWarn  = True
+    CMap      = 'viridis'
+    CbarNorth = True
+    PlotType  = 'bicoh'
+    ScaleAxes = 'manual'
+    Verbose   = True
+    Detrend   = False
+    ZPad      = False
+    Cross     = False
+    Vector    = False
+    TZero     = 0
+    tv = [] # Time vector
+    fv = [] # Frequency vector
+    ff = [] # Full frequency vector
+    ft = [] # Fourier amplitudes
+    sg = [] # Spectrogram (complex)
+    xs = [] # Cross-spectrum
+    xc = [] # Cross-coherence
+    cs = [] # Coherence spectrum
+    bs = [] # Bispectrum
+    bc = [] # Bicoherence spectrum
+    bp = [] # Biphase proxy
+    bg = [] # Bispectrogram
+    er = [] # Mean & std dev of FFT
+    mb = [] # Mean b^2
+    sb = [] # Std dev of b^2
 
-# Sampling rate
-fS = 1000
-dt = 1/fS
+    # Functions
+    def __init__(bic,raw):
+    # ------------------
+    # Constructor
+    # ------------------
+        bic.raw = raw
 
-# Time base
-tmax = 10
-t = np.arange(0, tmax, dt)
-N = len(t)
+    def PlotTest(t,sig):
+    # ------------------
+    # Debugger for plots
+    # ------------------
+        import matplotlib.pyplot as plt
 
-# Frequency domain
-df = 1/(dt*N)
-f = np.arange(0, fS, df) 
+        plt.plot(t,sig)
+        plt.xlabel("Time [Hz]")
+        plt.ylabel("Amplitude [arb.]")
+        plt.title("Test signal!")
+        plt.show()
 
-# Frequency
-w = 10;
 
-# Signal
-sig = N*[0]
-#sig = np.sin(w*2*np.pi*t) + np.sin(2*w*2*np.pi*t)/2 + np.sin(3*w*2*np.pi*t)/3
-for k in range(10):
-    sig += np.sin( (k+1) * w * 2*np.pi * t )/(k+1)
+    def SignalGen(fS,tend,Ax,fx,Afx,Ay,fy,Afy,Az,Ff,noisy):
+    # ------------------
+    # Provides FM test signal
+    # ------------------
+        import numpy as np
+        
+        t = np.arange(0,tend,1/fS)  # Time-vector sampled at "fS" Hz
 
-# FFT
-ft = np.fft.fft(sig)/N
+        # Make 3 sinusoidal signals...
+        dfx = Afx*np.sin(2*np.pi*t*Ff)  
+        dfy = Afy*np.cos(2*np.pi*t*Ff)
+        x = Ax*np.sin( 2*np.pi*(fx*t + dfx) )              # f1
+        y = Ay*np.sin( 2*np.pi*(fy*t + dfy) )              # f2
+        z = Az*np.sin( 2*np.pi*(fx*t + fy*t + dfx + dfy) ) # f1 + f2
 
-fig  = plt.figure()
-ax   = fig.add_subplot(111)
+        sig = x + y + z + noisy*(0.5*np.random.random(len(t)) - 1)
 
-# Plot
-plt.plot(f,np.abs(ft))
-plt.yscale("log")
+        return sig, t
 
-ax.set_xlim(0, fS/2)
-ax.set_ylim(0.0001, 1)
-
-plt.xlabel("Frequency [Hz]")
-plt.ylabel("|P|")
-
-plt.show()
+        
