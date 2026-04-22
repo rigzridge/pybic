64ba52e6089e0bc6489fb2f97a1cc8072d8a7da1
rigzridge <75751275+rigzridge@users.noreply.github.com>
2026-04-22 12:40:04 -0400

    including extensions from example

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,21 @@ release = '2.1.0'
 # 'sphinx.ext.autodoc' - to generate docs from docstrings
 # 'sphinx.ext.napoleon' - for Google/NumPy style docstrings
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
 ]
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
 
 # Should generate autosummary?
 autosummary_generate = True
