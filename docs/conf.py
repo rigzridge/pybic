e9fb3305318f5eb95edc4338f34ee924faa7a082
rigzridge <75751275+rigzridge@users.noreply.github.com>
2026-04-22 12:07:39 -0400

    setting up google style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -1,35 +1,39 @@
 # -- Project information -----------------------------------------------------
 project = 'PyBic'
-copyright = '2023-2026, G. Riggs and T. Matheny'
+copyright = '2022-2026, G. Riggs and T. Matheny'
 author = 'Greg Riggs'
 
-# The full version, including alpha/beta/rc tags
-release = '0.1.0'
+# Version #
+release = '2.1.0'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. 
-# Common extensions include:
+# Sphinx extension modules
 # 'sphinx.ext.autodoc' - to generate docs from docstrings
 # 'sphinx.ext.napoleon' - for Google/NumPy style docstrings
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
+# Not really using this yet...
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
+# ... or this lol
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages. 
 # 'sphinx_rtd_theme' is the standard theme for Read the Docs.
+# Will not work w/o this!
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. 
+# Not used yet!
 html_static_path = ['_static']
